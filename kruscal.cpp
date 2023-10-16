e3dc3144d565155997207e66278a7de5b69454e7
tom-e666 <tptien.a1.c3tqcap@gmail.com>
2023-10-16 15:35:12 +0700

    2

--- kruscal.cpp
+++ kruscal.cpp
@@ -26,34 +26,34 @@ void readgraph(const string &name, graph &g)
     }
     f >> g.dinh;
     for (int i = 1; i <= g.dinh; ++i)
-        for (int j = 1; j <= g.dinh; ++j)
+        for (int j = 0; j <= g.dinh; ++j)
         {
             f >> g.canh[i][j];
-            if (g.canh[i][j])
+            if (g.canh[i][j] && i < j)
                 g.edge_list.push_back({i, j, g.canh[i][j]});
         }
 }
 bool cochutrinh(graph g, int iMin, vector<edge> e, int Nhan[])
 {
     if (Nhan[e[iMin].u] == Nhan[e[iMin].v])
-        return 0;
+        return 1;
     else
     {
         int tmp = Nhan[e[iMin].v];
         for (int i = 0; i < iMin; i++)
         {
-            if (Nhan[e[i].v] == tmp)
+            if (Nhan[i] == tmp)
             {
-                Nhan[e[i].v] = Nhan[e[iMin].u];
+                Nhan[i] = Nhan[e[iMin].u];
             }
         }
-        return 1;
+        return 0;
     }
 }
 void kruscal(const graph &g, edge T[], int Nhan[], int &nT)
 {
 
-    for (int i = 1; i <= g.dinh; ++i)
+    for (int i = 0; i <= g.dinh; ++i)
         Nhan[i] = i;
     vector<edge> e(g.edge_list);
     sort(begin(e), end(e));
@@ -80,7 +80,7 @@ int main()
     graph g;
     edge T[1000];
     int Nhan[1000];
-    int nT;
+    int nT = 0;
     readgraph("input.txt", g);
     kruscal(g, T, Nhan, nT);
     for (int i = 0; i < nT; ++i)
